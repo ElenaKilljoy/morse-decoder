df8fa7b01492d42c5680c4ca422dc0e455ae0f99
ElenaKilljoy <lenokcardio@gmail.com>
2023-01-12 12:44:31 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    let char = '';
+    let morseCode = '';
+    let i = expr.length - 1;
+    let x = 0;
+    while (i > 0) {
+      char = expr[i - 9] + expr[i - 8] + expr[i - 7] + expr[i - 6] + expr[i - 5] + expr[i - 4] + expr[i - 3] + expr[i - 2] + expr[i - 1] + expr[i] + char;
+      if (char === '**********') {
+        result = ' ' + result;
+      } else {
+          while (x < char.length) {
+            if (char[x] + char[x + 1] === '10') {
+              morseCode = morseCode + '.';
+            } else if (char[x] + char[x + 1] === '11') {
+              morseCode = morseCode + '-';
+            } else {
+              morseCode = morseCode + '';
+            }
+            x = x + 2;
+          }
+          for (let key in MORSE_TABLE) {
+            if (key === morseCode) {
+              result = MORSE_TABLE[key] + result;
+            }
+          }
+        }
+      i = i - 10;
+      x = 0;
+      char = '';
+      morseCode = '';
+    }
+    return result;
 }
 
 module.exports = {
